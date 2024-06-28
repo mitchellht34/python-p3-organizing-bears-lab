ca56ac4f7e0abe6a8d494a108faf990c352947c2
Mitch Toran <mitchellht34@gmail.com>
2024-06-28 02:27:43 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 4, 'M', 'red', 'choleric', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 8, 'M', 'orange', 'sanguine', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 15, 'F', 'yellow', 'melancholic', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 16, 'M', 'brown', 'phlegmatic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 23, 'F', 'green', 'sanguine', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 42, 'M', 'blue', 'choleric', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'indigo', 'melancholic', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, 'F', 'violet', 'phlegmatic', 0);
